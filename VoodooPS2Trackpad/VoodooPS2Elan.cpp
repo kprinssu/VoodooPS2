7f774d3641c8e7ac0f743ab6615605f292ea8aaf
Kishor Prins <me@68x.ca>
2020-02-20 20:11:52 -0500

    Adding Elan class and Linux headers

---
+++ VoodooPS2Trackpad/VoodooPS2Elan.cpp
@@ -0,0 +1,9 @@
+//
+//  VoodooPS2Elan.cpp
+//  VoodooPS2Trackpad
+//
+//  Created by Kishor Prins on 2020-02-20.
+//  Copyright © 2020 Acidanthera. All rights reserved.
+//
+
+#include "VoodooPS2Elan.hpp"
